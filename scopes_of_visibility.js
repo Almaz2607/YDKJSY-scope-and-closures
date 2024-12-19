a3b5544c2d82d54bbb4bfa34a178bba4b950aded
Almaz Kubakaev <almazkubakaev2607@gmail.com>
2024-12-19 18:04:42 +0600

    scope-part2

--- scopes_of_visibility.js
+++ scopes_of_visibility.js
@@ -22,3 +22,53 @@ function getStudentName(studentID) {
 var nextStudent = getStudentName(24);
 
 console.log(nextStudent);
+
+// ---variable-shadowing----------------
+// --example-1--------------------------
+var personName = 'Patrick';
+
+function printPerson(personName) {
+    personName = personName.toUpperCase();
+    console.log(personName);
+}
+
+printPerson('Alice');
+// ALICE
+console.log(personName);
+// Patrick
+printPerson(personName);
+// PATRICK
+console.log(personName);
+// Patrick
+
+// --example-2-----------------------------
+var special = 77;
+
+function lookingFor(special) {
+    // identifier 'special' (parameter) in this scope
+    // is substituted inside keepLooking() and is
+    // therefore not accessible from this scope
+    function keepLooking() {
+        var special = 3.141592;
+        console.log(special);
+        console.log(window.special);
+    }
+
+    keepLooking();
+}
+
+lookingFor(557799);
+// 3.141592
+// 77
+
+// ---reverse-shadowing-----------------
+var someName = 'John';
+
+function printSomeName(someName) {
+    console.log(someName);
+    console.log(window.someName);
+}
+
+printSomeName('Elizabeth');
+// Elizabeth
+// John
